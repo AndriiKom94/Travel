7760c721222384250c794ceb3dfd1aa68ea50612
AndriiKom94 <andrii.kom@ukr.net>
2020-09-04 10:58:49 +0300

    add forms

--- cities/urls.py
+++ cities/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, CityDetailView
 
 urlpatterns = [
-    #path('', home, name='home'),
+    path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 
 def home(request, pk=None):
-    if pk:
-        city = City.objects.filter(id=pk).first()
-        return render(request, 'cities/detail.html', {'object': city})
+    #if request.method == 'POST':
+    #    print(request.POST)
+    #    print('*' * 10)
+    #    print(request.POST.get('name'))
+    #if pk:
+    #    city = City.objects.filter(id=pk).first()
+    #    return render(request, 'cities/detail.html', {'object': city})
+    #cities = City.objects.all()
+    #context = {'objects_list': cities, }
+    #return render(request, 'cities/home.html', context)
+    if request.method == 'POST':
+        form = CityForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+            name = form.cleaned_data.get('name')
+            city = City(name=name)
+            city.save()
+    form = CityForm()
     cities = City.objects.all()
-    context = {'objects_list': cities, }
+    context = {'objects_list': cities, 'form': form}
     return render(request, 'cities/home.html', context)
+
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
