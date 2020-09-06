8c4788922adca327b911a334d99e9117df7d7fde
AndriiKom94 <andrii.kom@ukr.net>
2020-09-06 23:39:47 +0300

    add delete and create mode

--- cities/urls.py
+++ cities/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, CityDetailView
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('add/', CityCreateView.as_view(), name='add'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
@@ -33,3 +34,23 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     context_object_name = 'object'
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
