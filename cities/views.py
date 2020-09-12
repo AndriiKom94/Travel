d361a7e0528c046c09b0b4a77e2f30293b17afd8
AndriiKom94 <andrii.kom@ukr.net>
2020-09-12 20:36:20 +0300

    add pagination

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
@@ -17,16 +18,22 @@ def home(request, pk=None):
     #cities = City.objects.all()
     #context = {'objects_list': cities, }
     #return render(request, 'cities/home.html', context)
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-            name = form.cleaned_data.get('name')
-            city = City(name=name)
-            city.save()
-    form = CityForm()
+    # if request.method == 'POST':
+    #    form = CityForm(request.POST or None)
+    #    if form.is_valid():
+    #        print(form.cleaned_data)
+    #        name = form.cleaned_data.get('name')
+    #        city = City(name=name)
+    #        city.save()
+    # form = CityForm()
+    # cities = City.objects.all()
+    # context = {'objects_list': cities, 'form': form}
+    # return render(request, 'cities/home.html', context)
     cities = City.objects.all()
-    context = {'objects_list': cities, 'form': form}
+    paginator = Paginator(cities, 4)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    context = {'objects_list': cities, }
     return render(request, 'cities/home.html', context)
 
 
