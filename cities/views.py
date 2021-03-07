d1c447046679a5363660b6bfeee22062f9166197
AndriiKom94 <63592072+AndriiKom94@users.noreply.github.com>
2021-03-07 19:07:42 +0200

    Update views.py
    cleaned code

--- cities/views.py
+++ cities/views.py
@@ -9,27 +9,6 @@ from cities.models import City
 
 
 def home(request, pk=None):
-    #if request.method == 'POST':
-    #    print(request.POST)
-    #    print('*' * 10)
-    #    print(request.POST.get('name'))
-    #if pk:
-    #    city = City.objects.filter(id=pk).first()
-    #    return render(request, 'cities/detail.html', {'object': city})
-    #cities = City.objects.all()
-    #context = {'objects_list': cities, }
-    #return render(request, 'cities/home.html', context)
-    # if request.method == 'POST':
-    #    form = CityForm(request.POST or None)
-    #    if form.is_valid():
-    #        print(form.cleaned_data)
-    #        name = form.cleaned_data.get('name')
-    #        city = City(name=name)
-    #        city.save()
-    # form = CityForm()
-    # cities = City.objects.all()
-    # context = {'objects_list': cities, 'form': form}
-    # return render(request, 'cities/home.html', context)
     cities = City.objects.all()
     paginator = Paginator(cities, 4)
     page = request.GET.get('page')
